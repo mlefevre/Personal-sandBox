f548c78b0c7c95158dbfeb10cfff7fe9bef232f3
Marc Lefèvre <mlefevre@cirb.irisnet.be>
2019-07-23 14:44:59 +0200

    New modification not to be reverted
    This modificaction affects an other part of the file

--- testFile.sh
+++ testFile.sh
@@ -4,6 +4,10 @@
 
 echo hello world !
 
+# This fourth modification (after a deletion) is before a part deleted which will be restored through a revert.
+
 # This a first modification after creation
 
 # This third modification should not be lost when I'll revert the previous commit
+
+

3fc60d959fd8269d5292a048e1b400025bff6619
Marc Lefèvre <mlefevre@cirb.irisnet.be>
2019-07-23 14:45:16 +0200

    Revert "suppression to be reverted"
    This reverts commit 15b0ebc1bc83339ca1d98573b5621264f666788f.

--- testFile.sh
+++ testFile.sh
@@ -8,6 +8,8 @@ echo hello world !
 
 # This a first modification after creation
 
+# This is a second modification. It will be reverted soon ...
+
 # This third modification should not be lost when I'll revert the previous commit
 
 
